654810635451b5dea7fbc8914be76d2c97f622f2
SaiMukZi <saimukzi@hiauntie.com>
2025-01-12 22:51:50 +0800

    cv2_imwrite retry

--- common.py
+++ common.py
@@ -2,6 +2,7 @@ import cv2
 import my_logger
 import numpy as np
 import os
+import time
 
 def find_file(folder_path):
     for root, dirs, files in os.walk(folder_path):
@@ -15,10 +16,16 @@ def cv2_imread(file_path, flags=cv2.IMREAD_COLOR, *args, **kwargs):
         return cv2.imdecode(np.frombuffer(bytess, np.uint8), flags=flags, *args, **kwargs)
 
 def cv2_imwrite(file_path, img, params=None):
-    try:
-        bytess = cv2.imencode('.png', img, params=params)[1].tobytes()
-        with open(file_path, 'wb') as f:
-            f.write(bytess)
-    except:
-        my_logger.logger.error(f'FGRISMSNBX Error in cv2_imwrite: {file_path}')
-        raise
+    retry_remain = 5
+    while True:
+        try:
+            bytess = cv2.imencode('.png', img, params=params)[1].tobytes()
+            with open(file_path, 'wb') as f:
+                f.write(bytess)
+            break
+        except:
+            retry_remain -= 1
+            my_logger.logger.error(f'FGRISMSNBX Error in cv2_imwrite: {file_path}, retry_remain: {retry_remain}')
+            if retry_remain <= 0:
+                raise
+            time.sleep(0.1)
